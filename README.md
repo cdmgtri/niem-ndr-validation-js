f6e5ef0c827f4930748dd6d08dc7130086b80b4d
CDM <>
2019-04-26 13:22:34 -0400

    Validate multiple hard-coded local files. Additional test files. Results show line numbers.

--- README.md
+++ README.md
@@ -1,16 +1,15 @@
 
 # NIEM JS - NDR Validation
 
-## Steps
+Run NDR conformance rules against XML schemas.
 
-- [ ] Inputs
-  - [ ] NIEM XSD file
-  - [ ] NDR XSL rule file
-- [ ] Run XSLT transformation => SVRL
-- [ ] For each failed assert
-  - [ ] get the file name
-  - [ ] get the component kind
-  - [ ] get the component name
+## Status
+
+This project uses Saxon-JS to transform NIEM XML schemas with pre-compiled NDR XSL rules (compiled via Oxygen) to generate a list of conformance issues.  Saxon-JS is currently only implemented for browser support; a Node.js version is projected to be released this year.  At that point, this code can be refactored so that NDR validation can be run offline, via node, a npm script, Travis, etc.
+
+- [x] Local web server to provide hard-coded input file access
+- [x] Validate XSDs against pre-compiled 4.0 EXT NDR rules.
+- [x] Parse resulting SVRL to display results
 - [ ] Save conformance report
-- [ ] Generate badge info
-- [ ] Create badge
+- [ ] Create NDR badge
+- [ ] Refactor Saxon-JS for Node.js implementation once available
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,135 @@
 
-function test(msg) {
-  console.log("******", msg, "******");
+let xsdDocuments = {};
+let textDocuments = {};
+
+/**
+ * Tests each XSD in the base-xsd/extension directory
+ */
+function validateExtensionSchemas() {
+  validateFile("http://localhost:8080/base-xsd/extension/extension.xsd");
+  validateFile("http://localhost:8080/base-xsd/extension/test-1.xsd");
+  validateFile("http://localhost:8080/base-xsd/extension/test-2.xsd");
 }
 
 /**
- * @param {HTMLElement} xsdDocument
+ * Adds the file name from the given xsdPath to the status page element as loading.
+ * Loads the XSD file at the given xsdPath as an XML document.
+ * Starts the XSLT transformation.
+ *
+ * @param {string} xsdPath
+ */
+function validateFile(xsdPath) {
+
+  let fileName = xsdPath.split("/").pop();
+
+  // Set status for the file
+  addStatus(fileName, "", "Loading...");
+
+  // Load the input schema
+  loadSchema(xsdPath, fileName);
+
+  let ndrPath = "http://localhost:8080/assets/ndr/ndr-rules-conformance-target-ext.sef";
+
+  // Run the XSLT to get conformance test results
+  SaxonJS.transform({
+      stylesheetLocation: ndrPath,
+      sourceLocation: xsdPath
+    },
+    resultElement => {
+
+      // Save results to file
+      let svrl = resultElement.outerHTML;
+      let blob = new Blob([svrl], {type: "text/plain;charset=utf-8"});
+      // saveAs(blob, "results.svrl");
+
+      // Parse the SVRL results of the XSLT transform
+      let issueCount = parseIssues(fileName, resultElement, "log");
+
+      // Remove loading message
+      updateStatus(fileName, "NDR 4.0 EXT", issueCount);
+
+    });
+}
+
+function addStatus(fileName, ruleSet, status) {
+
+  let cellFileName = document.createElement("td");
+  let cellRuleSet = document.createElement("td");
+  let cellStatus = document.createElement("td");
+
+  cellFileName.innerHTML = fileName;
+  cellRuleSet.innerHTML = ruleSet;
+  cellStatus.innerHTML = status;
+
+  let tbl = document.getElementById("status").children[0];
+  let row = document.createElement("tr");
+
+  row.setAttribute("id", "status." + fileName);
+  row.setAttribute("class", "table-warning");
+  row.appendChild(cellFileName);
+  row.appendChild(cellRuleSet);
+  row.appendChild(cellStatus);
+
+  tbl.appendChild(row);
+}
+
+/**
+ * Update the file status with a pass/fail message and row highlighting.
+ *
+ * @param {string} fileName
+ * @param {string} ruleSet
+ * @param {number} issueCount
+ */
+function updateStatus(fileName, ruleSet, issueCount) {
+
+  let row = document.getElementById("status." + fileName);
+
+  let msg = "All tests passed";
+  let style = "table-success";
+
+  if (issueCount != 0) {
+    msg = issueCount + " tests failed";
+    style = "table-danger";
+  }
+
+  // Update cell values
+  row.children[1].innerHTML = ruleSet;
+  row.children[2].innerHTML = msg;
+
+  // Set row style
+  row.setAttribute("class", style);
+}
+
+
+function loadSchema(xsdPath, fileName) {
+
+  let schemaFile = new XMLHttpRequest();
+  schemaFile.open("GET", xsdPath, true);
+  schemaFile.send(null);
+
+  schemaFile.onreadystatechange = function() {
+    if (schemaFile.readyState === 4 && schemaFile.status == 200) {
+      xsdDocuments[fileName] = schemaFile.responseXML;
+      textDocuments[fileName] = schemaFile.responseText;
+    }
+  };
+}
+
+/**
+ * @param {string} fileName
  * @param {HTMLElement} svrl
  * @param {string} tableID
- * @param {string} summaryID
  */
-function parseIssues(xsdDocument, svrl, tableID, summaryID) {
+function parseIssues(fileName, svrl, tableID) {
 
   let table = document.getElementById(tableID).firstElementChild;
 
   let failedAsserts = svrl.getElementsByTagName("svrl:failed-assert");
 
   for (let failedAssert of failedAsserts) {
+
+    /** @type {HTMLElement} */
+    let xsdDocument = xsdDocuments[fileName];
 
     // Get the matching component node from the XSD document
     let xPath = failedAssert.attributes["location"].nodeValue;
@@ -28,10 +142,29 @@ function parseIssues(xsdDocument, svrl, tableID, summaryID) {
     let [rule, desc] = msg.split(": ");
     rule = rule.replace("Rule ", "");
 
+    // Get the line number
+    let re = new RegExp(`${kind}\\s*name=.${name}`)
+    let lineNumber = getLineNumber(textDocuments[fileName], re);
+
     // Add row to the issue list
-    addRow(table, "FILE NAME", "#", kind, name, rule, desc);
+    addRow(table, fileName, lineNumber, kind, name, rule, desc);
   }
 
+  return failedAsserts.length;
+
+}
+
+/**
+ * Given a document string, counts the number of '\n' from the start to the
+ * position of the given pattern.
+ *
+ * @param {string} documentString
+ * @param {RegExp} pattern
+ */
+function getLineNumber(documentString, pattern) {
+  let index = documentString.search(pattern);
+  let leadingText = documentString.substring(0, index);
+  return leadingText.split("\n").length;
 }
 
 /**
@@ -42,7 +175,6 @@ function parseIssues(xsdDocument, svrl, tableID, summaryID) {
  * @returns {HTMLElement}
  */
 function getComponentNode(xsdDocument, xpath) {
-
   let xPathResult = document.evaluate(xpath, xsdDocument);
   let node = xPathResult.iterateNext();
   return getComponentRootNode(node);
